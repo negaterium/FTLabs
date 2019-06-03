33796a4cc539b3449c68dd306417f9f99e5d1315
Lucian Pop <negaterium@yahoo.com>
2019-06-03 22:09:58 +0300

    Add T2 - p6-p9

--- src/Lab2/TemaLab2.java
+++ src/Lab2/TemaLab2.java
@@ -29,21 +29,57 @@ public class TemaLab2 {
         System.out.println("Impartire a doua numere = " + impartireNumere(667, 6));
 
         //Tema p.3 - Printing a specific model -JAVA
+        System.out.println("------Printing JAVA text model");
         printJavaModel();
 
         //Tema p.4 - Tiparire medie numere naturale
         System.out.println("Media a 3 numere naturale = " + mediaNumere(123, 234.5f, 556));
 
         //Tema p.5 - Printing a specific model -Head
+        System.out.println("------Printing Head text model");
         printHeadModel();
+
+        //Tema p.6 - Restul impartiri a 2 numere
+        System.out.println("Restul impartirii a lui 1234 cu 321 este:" + restImpartire(1234, 321));
+
+        //Tema p.7 - Transformare grade din F in C
+        System.out.println("Transformare temp din 102 F in Celsius ->: " + transformFahToCel(102));
+
+        //Tema p.8 - Transformare inch to meters
+        System.out.println("Transformare 123 inch to metters ->: " + transformInchToMeters(123));
+
+        //Tema p.9 - Show speed
+        showSpeedBasedOnParamters(20, 59, 1, 100000.0);
+
 
     }
 
-    public static void printHeadModel() {
-        printModel("   J    a    v     v  a");
-        printModel("   J   a a    v   v  a  a");
-        printModel("J  J  aaaaa    v v  aaaaaa");
-        printModel(" JJ  a     a    v  a      a");
+    public static void showSpeedBasedOnParamters(int timeSec, int timeMin, int timeHours, double distanceMeters) {
+        int totalSeconds = timeSec + timeMin * 60 + timeHours * 3600;
+        double totalHours = totalSeconds / 3600.0;
+        double totalKM = distanceMeters / 1000.0;
+        double totalMiles = distanceMeters / 1609.0;
+        System.out.println("Distance in km = " + totalKM);
+        System.out.println("Speed in meters/second = " + (distanceMeters / totalSeconds));
+        System.out.println("Speed in km/hour = " + (totalKM / totalHours));
+        System.out.println("Speed in miles/hour = " + (totalMiles / totalHours));
+    }
+
+    public static double transformInchToMeters(float inch) {
+        double centimeters = inch * 2.5;
+        double meters = centimeters / 100;
+        return meters;
+    }
+
+    public static double transformFahToCel(int temp) {
+        double partial = 5.0 / 9;
+        double tempC = partial * (temp - 32);
+        System.out.println(tempC);
+        return tempC;
+    }
+
+    public static int restImpartire(int f1, int f2) {
+        return f1 % f2;
     }
 
     public static void printJavaModel() {
@@ -51,6 +87,15 @@ public class TemaLab2 {
         printModel("   J   a a    v   v  a  a");
         printModel("J  J  aaaaa    v v  aaaaaa");
         printModel(" JJ  a     a    v  a      a");
+    }
+
+    public static void printHeadModel() {
+        printModel(" +\"\"\"'\"+");
+        printModel("[| o o |]");
+        printModel(" |  ^  |");
+        printModel(" |     |");
+        printModel(" | '_' |");
+        printModel(" +-----+");
     }
 
     public static float mediaNumere(float n1, float n2, float n3) {
